e9fe395fceb80a24b85bc1b4949b3f3155f69452
itsmekhalidrizky <khalidrizkyfirdaus@gmail.com>
2022-07-31 22:13:20 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:campusacademy_app/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const CampusAcademyApp());
 }
 
